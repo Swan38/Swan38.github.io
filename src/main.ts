699f2f82a748568be80422757b2448de4e198bf5
Swan AUVERGNE <swan.auvergne@gmail.com>
2025-01-15 00:08:14 +0100

    Tutorial and data storage

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import SVGInjector from "svg-injector"
 import './style.css'
 
 import { ParticipantListUI } from "./ui"
-
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.ts'
+import { is_data_stored } from "./data"
 
 
 SVGInjector(document.querySelectorAll(`img[class="tab_selection_image"]`))
@@ -14,22 +11,35 @@ SVGInjector(document.querySelectorAll(`img[class="tab_selection_image"]`))
 const container_participants: HTMLElement = document.getElementById(`container_participants`)!
 container_participants.insertAdjacentElement('beforeend', new ParticipantListUI().get_elem())
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+const tutorial: HTMLDivElement = document.getElementById('tutorial') as HTMLDivElement
+const tab_layout: HTMLDivElement = document.getElementById('tab_layout') as HTMLDivElement
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+{ // Tutorial or main app
+    if (is_data_stored()) // TODO if data stored
+        tutorial.style.display = 'none';
+    else
+        tab_layout.style.display = 'none';
+
+    (document.getElementById('start_button') as HTMLButtonElement).onclick = () => {
+        tutorial.classList.add('disappear')
+        setTimeout(() => {
+            tutorial.style.display = 'none'
+            tab_layout.style.display = ''
+            tab_layout.classList.add('appear')
+        }, 500)
+    }
+}
+
+{ // Frop file area
+    const drop_area: HTMLLabelElement = document.getElementById('file_input_label') as HTMLLabelElement
+    const drop_input: HTMLInputElement = drop_area.querySelector('input[type="file"]')!
+
+    drop_area.ondragenter = () => { drop_area.setAttribute('hover_drop', '') }
+    drop_area.ondragleave = (event) => { console.log(event); drop_area.removeAttribute('hover_drop') }
+    drop_area.ondragover = drop_area.ondragenter = (event) => { event.preventDefault() }
+
+    drop_area.ondrop = (event) => {
+        drop_input.files = event.dataTransfer?.files || null
+        event.preventDefault()
+    }
+}
